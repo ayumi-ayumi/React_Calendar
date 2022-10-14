88c05f804311d2efd8f0f434a4bba3db9cbd0367
Ayumi <ayuayuayuayus@gmail.com>
2022-10-14 12:01:40 +0200

    responsive done

--- src/Components/DaysOfWeek.js
+++ src/Components/DaysOfWeek.js
@@ -2,14 +2,26 @@ import React from 'react'
 
 export default function DaysOfWeek () {
   return (
-    <div className='daysOfWeek'>
-        <div>Mon</div>
-        <div>Tue</div>
-        <div>Wed</div>
-        <div>Thu</div>
-        <div>Fri</div>
-        <div>Sat</div>
-        <div>Sun</div>
+    <div>
+      <div className='daysOfWeek'>
+          <div>Mon</div>
+          <div>Tue</div>
+          <div>Wed</div>
+          <div>Thu</div>
+          <div>Fri</div>
+          <div>Sat</div>
+          <div>Sun</div>
+      </div>
+      <div className='daysOfWeekShort'>
+          <div>M</div>
+          <div>T</div>
+          <div>W</div>
+          <div>T</div>
+          <div>F</div>
+          <div>S</div>
+          <div>S</div>
+      </div>
     </div>
+
   )
 }
