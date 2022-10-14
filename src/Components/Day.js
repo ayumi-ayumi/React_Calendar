e544a6318400b9785d7536eecbbb3d697cd79137
Ayumi <ayuayuayuayus@gmail.com>
2022-10-14 11:52:31 +0200

    first attempt done

--- src/Components/Day.js
+++ src/Components/Day.js
@@ -4,8 +4,9 @@ export default function Day (props) {
 
   function showDay () {
     const dayArr = []
-    const blank =[]
-
+    const blankOfBeginning =[]
+    const blankOfEnd =[]
+    console.log(props.dayOfLastDay)
     //creating an array for days of the month
     for(let i=1; i <= props.howManyDays; i++) {
       dayArr.push(<div className='aDay' key={i}>{i}</div>)
@@ -14,48 +15,48 @@ export default function Day (props) {
     //filling up days before the first day 
     if(props.dayOfFirstDay===0){
       for(let j=0; j < 6; j++) {
-        blank.push(<div className='blank'></div>)
+        blankOfBeginning.push(<div className='blank'></div>)
       } 
-      dayArr.unshift(blank)
+      dayArr.unshift(blankOfBeginning)
 
     } else {
-      for(let j=0; j < props.dayOfFirstDay-1; j++) {
-        blank.push(<div className='blank' key={j}></div>)
+      for(let p=0; p < props.dayOfFirstDay-1; p++) {
+        blankOfBeginning.push(<div className='blank' key={p}></div>)
       } 
-      dayArr.unshift(blank)
-      
+      dayArr.unshift(blankOfBeginning)
     }
     
-    //filling up days after the last day 
+    // filling up days after the last day 
     if(props.dayOfLastDay!==0){
-      for(let l=0; l <= 7-props.dayOfLastDay; l++) {
-        blank.push(<div className='blank' key={l+50}></div>)
+      for(let l=0; l < 7-props.dayOfLastDay; l++) {
+        blankOfEnd.push(<div className='blank' key={l+50}></div>)
       } 
-      dayArr.push(blank)
+      dayArr.push(blankOfEnd)
 
     } 
-    // else {
-    //   for(let j=0; j < props.dayOfLastDay-1; j++) {
-    //     blank.push(<div key={j}></div>)
-    //   } 
-    //   dayArr.push(blank)
-      
-    // }
-
-    
-    // if(props.dayOfFirstDay!==1){
-    //   for(let j=0; j < props.dayOfFirstDay-1; j++) {
-    //     blank.push(<div></div>)
-    //   } 
-    //   dayArr.unshift(blank)
-    // } 
     return <div  className='days'>{dayArr}</div>
-
+    
   }
-
-    return (
-      <div>
+  
+  return (
+    <div>
       {showDay()}
       </div>
   )
 }
+
+// else {
+//   for(let j=0; j < props.dayOfLastDay-1; j++) {
+//     blank.push(<div key={j}></div>)
+//   } 
+//   dayArr.push(blank)
+  
+// }
+
+
+// if(props.dayOfFirstDay!==1){
+//   for(let j=0; j < props.dayOfFirstDay-1; j++) {
+//     blank.push(<div></div>)
+//   } 
+//   dayArr.unshift(blank)
+// } 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,18 +1,20 @@
-import React from "react"
+import React from "react";
+import leftArrow from "/Users/Ayumi/Desktop/Frauenloop/React/react_calendar/src/left_arrow.png";//why not relative path works??
+import rightArrow from "/Users/Ayumi/Desktop/Frauenloop/React/react_calendar/src/right_arrow.png";
 
 export default function Header(props) {
     const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
     
     return (
         <div className="header">
-            <button onClick={props.onClickPrevMonth}>Prev Month</button>
+            <button onClick={props.onClickPrevMonth}><img src={leftArrow} alt="left_arrow"/></button>
             <div className="month">
                 {months[props.monthNumber]}
             </div>
             <div  className="year">
                 {props.yearNumber}
             </div>
-            <button onClick={props.onClickNextMonth}>Next Month</button>
+            <button onClick={props.onClickNextMonth}><img src={rightArrow} alt="left_arrow"/></button>
         </div>
     )
 }
